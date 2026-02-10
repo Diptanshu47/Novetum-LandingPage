e77ac4f791dda5d95b0cac2c8c6a44fb5f2f1d9b
Diptanshu <your-github-email@example.com>
2026-02-10 19:44:53 +0530

    yaml and config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,20 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig = {
 	output: "export",
 	trailingSlash: true,
-
 	images: {
 		unoptimized: true,
 	},
-
-	// Only use subpath on GitHub Pages
-	basePath: isProd ? "/Novetum-LandingPage" : "",
-	assetPrefix: isProd ? "/Novetum-LandingPage/" : "",
-
+	// Only use subpath for GitHub Pages
+	basePath: isGitHubPages ? "/Novetum-LandingPage" : "",
+	assetPrefix: isGitHubPages ? "/Novetum-LandingPage/" : "",
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-
 	webpack(config) {
 		const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
-
 		config.module.rules.push(
 			{
 				...fileLoaderRule,
@@ -36,10 +30,8 @@ const nextConfig = {
 				use: ["@svgr/webpack"],
 			},
 		);
-
 		fileLoaderRule.exclude = /\.svg$/i;
 		return config;
 	},
 };
-
 export default nextConfig;
